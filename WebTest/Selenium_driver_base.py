ea511f151f13f97f677eabcc2e16c33176be3828
adonet <duwenchao2003@163.com>
2020-02-01 23:13:57 +0800

    添加Linux-Selenium路径

--- WebTest/Selenium_driver_base.py
+++ WebTest/Selenium_driver_base.py
@@ -11,7 +11,8 @@ def selenium_driver():
         # driver = webdriver.Chrome(executable_path=driver_path)
         return driver_path
     elif os_name == 'Linux':
-        print("Linux操作系统，暂未配置driver路径")
+        driver_path = r"/usr/bin/chromedriver"
+        print("Linux操作系统，driver路径已配置，路径为：{}".format(driver_path))
     elif os_name == 'Darwin':
         driver_path = r"/Applications/Google Chrome.app/Contents/MacOS/chromedriver"
         print("MacOS操作系统，driver路径已配置，路径为：{}".format(driver_path))
@@ -19,3 +20,4 @@ def selenium_driver():
         return driver_path
     else:
         print("其他操作系统，暂未配置driver路径")
+
